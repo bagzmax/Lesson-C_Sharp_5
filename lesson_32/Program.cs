3a8b8920d5bbb870ace3614440515b60ded187cd
Maksim <bagzmax75@gmail.com>
2022-07-09 22:41:05 +0300

    add commit

---
+++ lesson_32/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+Console.Clear();
+
+int[] array = new int[4] {-4, -8, 8, 2};
+
+GetSignArray(array);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+void GetSignArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] *= -1;
+    }
+}
