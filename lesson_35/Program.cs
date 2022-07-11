224c6d9420fcbd1bb05b96d620f1a45d24cb23ce
Maksim <bagzmax75@gmail.com>
2022-07-12 01:28:20 +0300

    add lesson 35

--- lesson_35/Program.cs
+++ lesson_35/Program.cs
@@ -4,16 +4,30 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-int[] array = {5, 18, 123, 6, 2, 222};
+Console.Clear();
+
+int size = 123;
 int count = 0;
+int[] array = new int[size];
+GetArray(array);
+
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i] > 9 && array[i] < 199)
+    /* if (array[i] >= 10 && array[i] <= 99)
     {
         count += 1;
-    }
-    //count += (array(i) > 9 && array(i) < 100) ? 1 : 0;
+    } */
+    count += (array[i] >= 10 && array[i] <= 99) ? 1 : 0;
 }
 
-Console.WriteLine($"[{String.Join(",", array)}]");
-System.Console.WriteLine(count);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine(count);
+
+int[] GetArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(200);
+    }
+    return array;
+}
